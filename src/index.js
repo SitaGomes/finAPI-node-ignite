95030550942b724769f4a5a71098f693bee92993
SitaGomes <ansitagomes@gmail.com>
2022-05-21 10:07:28 +0000

    Add: POST -> Account creation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import express from 'express';
+import {v4 as uuidv4} from 'uuid'
 
 const app = express();
+app.use(express.json());
+
+const costumers = []
+
+app.post('/account', (req, res) => {
+    const {cpf, name} = req.body
+
+    const costumerAlreadyExists = costumers.some(
+        (costumers) => costumers.cpf === cpf 
+    )
+
+    if (costumerAlreadyExists) 
+        return res.status(400).json({Error: 'Customer already exists'})
+
+    costumers.push({
+        name, 
+        cpf,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+
+})
+
+app.get('/account', (req, res) => {
+    return res.status(201).send()
+})
 
 const PORT = 3333
 app.listen(PORT)
